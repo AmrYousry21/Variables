9caa34be9bdf0b7de3c5a02fd4f7ba5e2ed87966
Amr Yousry <amryousry19@gmail.com>
2022-08-04 10:14:51 -0400

    variables visual studio community

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "Amr";
+			int age = 26;
+			char middleInitial = 'H';
+			bool human = true;
+			double height = 5.7;
+			decimal dec = 590;
+
+			Console.WriteLine($"My Name is {name}, I'm {age} years old.");
+		}
+	}
+}
